5a8ca80398df716875ae6860e4c21c5491e7646e
Md. Abu Sufian Sani <66630062+sufian-sani@users.noreply.github.com>
2026-01-16 14:40:52 +0600

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,14 +1,9 @@
 // vars/docker_build.groovy
-
 def call(String projectName, String imageTag, String dockerHubUser) {
-    // Enable BuildKit for modern builds
     withEnv(["DOCKER_BUILDKIT=1"]) {
         sh """
             echo "Building Docker image: ${dockerHubUser}/${projectName}:${imageTag}"
-            docker buildx build \
-                --progress=plain \
-                -t ${dockerHubUser}/${projectName}:${imageTag} \
-                .
+            docker build -t ${dockerHubUser}/${projectName}:${imageTag} .
         """
     }
 }
